71fcdf2aa835ab2bec40491381f16f1aa29837fa
ariprahmanhakim98 <ariprahmanhakim90@gmail.com>
2023-06-09 22:42:07 +0700

    first commit

---
+++ application/views/monitoring.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Antrian</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <section id="header">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="border p-3 text-center">
+                    <div class="card-body">
+                        <h2>Klinik Rina Kasih Sodong</h2>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="row">
+                    <div class="col-sm-12 p-1 px-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div>ANTRIAN</div>
+                                <h1>A01</h1>
+                                <div>
+                                    <tr>
+                                        <td>Nama :</td>
+                                        <td>John Doe</td>
+                                    </tr>
+                                </div>
+                                <div>
+                                    <tr>
+                                        <td>Tujuan :</td>
+                                        <td>POLI UMUM</td>
+                                    </tr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 p-1 px-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div>ANTRIAN</div>
+                                <h1>A01</h1>
+                                <div>
+                                    <tr>
+                                        <td>Nama :</td>
+                                        <td>John Doe</td>
+                                    </tr>
+                                </div>
+                                <div>
+                                    <tr>
+                                        <td>Tujuan :</td>
+                                        <td>POLI UMUM</td>
+                                    </tr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 p-1 px-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div>ANTRIAN</div>
+                                <h1>A01</h1>
+                                <div>
+                                    <tr>
+                                        <td>Nama :</td>
+                                        <td>John Doe</td>
+                                    </tr>
+                                </div>
+                                <div>
+                                    <tr>
+                                        <td>Tujuan :</td>
+                                        <td>POLI UMUM</td>
+                                    </tr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-9 p-1">
+                <div class="row">
+                    <div class="col-sm-9">
+                        <div class="border">
+                            <div class="p-3">
+                                <video width="100%" height="100%" controls>
+                                    <source src="<?= base_url('assets/cinematic.mp4') ?>" type="video/mp4">
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="border">
+                            ANTRIAN
+                        </div>
+                    </div>
+                    <div class="col-sm-12 mt-2">
+                        <div class="border">
+                            ANTRIAN
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
