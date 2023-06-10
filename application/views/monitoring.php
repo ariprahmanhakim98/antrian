e4395e05fccbf635f25fa59567dd100fb030fd2f
ariprahmanhakim98 <ariprahmanhakim90@gmail.com>
2023-06-10 22:23:04 +0700

    add antrian

--- application/views/monitoring.php
+++ application/views/monitoring.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Antrian</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+    </style>
   </head>
   <body>
     <section id="header">
@@ -22,9 +27,9 @@
         <div class="row">
             <div class="col-sm-3">
                 <div class="row">
-                    <div class="col-sm-12 p-1 px-3">
-                        <div class="card">
-                            <div class="card-body text-center">
+                    <div class="col-sm-12">
+                        <div class="card m-1">
+                            <div class="text-center p-2">
                                 <div>ANTRIAN</div>
                                 <h1>A01</h1>
                                 <div>
@@ -44,9 +49,9 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-12 p-1 px-3">
-                        <div class="card">
-                            <div class="card-body text-center">
+                    <div class="col-sm-12">
+                        <div class="card m-1">
+                            <div class="text-center p-2">
                                 <div>ANTRIAN</div>
                                 <h1>A01</h1>
                                 <div>
@@ -66,9 +71,9 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-12 p-1 px-3">
-                        <div class="card">
-                            <div class="card-body text-center">
+                    <div class="col-sm-12">
+                        <div class="card m-1">
+                            <div class="text-center p-2">
                                 <div>ANTRIAN</div>
                                 <h1>A01</h1>
                                 <div>
@@ -88,10 +93,10 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-9 p-1">
+            <div class="col-sm-9">
                 <div class="row">
                     <div class="col-sm-9">
-                        <div class="border">
+                        <div class="border mt-1">
                             <div class="p-3">
                                 <video width="100%" height="100%" controls>
                                     <source src="<?= base_url('assets/cinematic.mp4') ?>" type="video/mp4">
@@ -101,13 +106,84 @@
                         </div>
                     </div>
                     <div class="col-sm-3">
-                        <div class="border">
-                            ANTRIAN
+                        <div class="border mt-1">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="text-center border m-2">
+                                        <div>INFO 1</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="col-sm-12 mt-2">
                         <div class="border">
-                            ANTRIAN
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="text-center border m-3">
+                                        <div>ANTRIAN</div>
+                                        <h3>B01</h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="text-center border m-3">
+                                        <div>ANTRIAN</div>
+                                        <h3>B01</h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="text-center border m-3">
+                                        <div>ANTRIAN</div>
+                                        <h3>B01</h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="text-center border m-3">
+                                        <div>ANTRIAN</div>
+                                        <h3>B01</h3>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
